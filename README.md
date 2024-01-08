f207f305addef7d34a33785f8f4ee3220a071632
DNSubramanyam <subbu.dns1998@gmail.com>
2024-01-09 02:09:08 +0530

    first successful test to fetch server list in lambda

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ec2-maintainence
 Lambda function name = ec2-maintenance-trigger
+LambdaRole = custom-ec2-maintanence-project-role
--- lambda_trigger.py
+++ lambda_trigger.py
@@ -1,6 +1,5 @@
 import json
 import boto3
-from pprint import pprint
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
@@ -9,12 +8,13 @@ def lambda_handler(event, context):
 
     instance_list = []
     f_tag =  {'Name': 'tag:maintenance-automation', 'Values':['weekday']}
-    
+
     ec2_response = ec2.describe_instances(Filters= [f_tag])['Reservations']
     for b in ec2_response:
         for c in b['Instances']:
             instance_list.append(c['InstanceId'])
     return {
         'statusCode': 200,
-        'body': json.dumps(event['action'])
+        'action': event['action'],
+        'instances': instance_list
     }
