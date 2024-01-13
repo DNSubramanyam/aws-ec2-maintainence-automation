0a0321a4b12f91ba1f7432de67e1b42f56e99878
DNSubramanyam <subbu.dns1998@gmail.com>
2024-01-13 17:35:53 +0530

    alarm fetch logic in lambda function

--- lambda_trigger.py
+++ lambda_trigger.py
@@ -32,12 +32,15 @@ def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
     asg = boto3.client('autoscaling', region_name='us-east-1')
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    cw = boto3.client('cloudwatch', region_name='us-east-1')
 
     table_name=event['TableName']
     db_table = dynamodb.Table(table_name)
     
     instance_list = []
+    instance_name = {}
     asg_list = []
+    alarm_list = []
 
     filter = get_fetch_tag(event['action'])
 
@@ -46,6 +49,10 @@ def lambda_handler(event, context):
         for b in ec2_response:
             for c in b['Instances']:
                 instance_list.append(c['InstanceId'])
+                instance_name.setdefault(c['InstanceId'], '')
+                for d in c['Tags']:
+                    if d['Key'] == 'Name':
+                        instance_name[c['InstanceId']] = d['Value']
 
     if len(instance_list) > 0:
         asg_response = asg.describe_auto_scaling_instances(InstanceIds = instance_list)['AutoScalingInstances']
@@ -54,16 +61,24 @@ def lambda_handler(event, context):
             to_write={
                 'InstanceId': a['InstanceId'],
                 'Timestamp': datetime.now().strftime("%d-%m-%Y_%H:%M:%S"),
-                'AutoScalingGroup': a['AutoScalingGroupName'],
+                'Name': instance_name[a['InstanceId']],
+                'AutoScalingGroup': a['AutoScalingGroupName']
+                
             }
             db_table.put_item(Item=to_write)
         asg_list = list(set(asg_list))
+
+        for id, name in instance_name.items():
+            alarm_response = cw.describe_alarms(AlarmNamePrefix=name+ '-' + id)['MetricAlarms']
+            for f in alarm_response:
+                alarm_list.append(f['AlarmName'])
 
     return {
         'statusCode': 200,
         'action': event['action'],
         'InstanceID': instance_list,
-        'ASG_list': asg_list
+        'ASG_list': asg_list,
+        'Alarm_list': alarm_list
     }
 
 if __name__ == "__main__":
